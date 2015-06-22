082bc7ad791c47f5240553779a125c784d3fd2d2
unknown <blue5@RM02-05.fasfg.local>
2015-06-22 13:32:23 +0100

    lesson 4 part 3

--- Lesson4/selection/Form1.cs
+++ Lesson4/selection/Form1.cs
@@ -25,15 +25,32 @@ namespace selection
         private void button1_Click(object sender, EventArgs e)
         {
             //1. Basic 'if' statement
-            if (comboBox1.Text == "Seo")
+            /*if (comboBox1.Text == "Seo")
             {
                 MessageBox.Show("You picked Seo");
                 comboBox1.Text = "";
             }
-            //2. One line of code to be excuted doesn't need curly braces
+            2. One line of code to be excuted doesn't need curly braces
             if (comboBox1.Text == "Seos")
                 MessageBox.Show("You picked Seos");
-
+            */
+            //3. nested 'if statement
+            if (comboBox1.Text != "Shozy")
+            {
+                if (comboBox1.Text == "Seo")
+                {
+                    MessageBox.Show("You picked Seo");
+                    comboBox1.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("I'm not sure who you picked.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("You picked shozy");
+            }
         }
     }
 }
