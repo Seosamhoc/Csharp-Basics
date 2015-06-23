c4b412194ea6ceb07439eb41315b6ff61f7e2819
unknown <blue5@RM02-05.fasfg.local>
2015-06-23 10:39:38 +0100

    Started project and created vehicle class

---
+++ Lesson5/OOP/vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Vehicle
+    {
+        public Vehicle()
+        {
+            System.Windows.Forms.MessageBox.Show("Called Constructor");
+        }
+    }
+}
