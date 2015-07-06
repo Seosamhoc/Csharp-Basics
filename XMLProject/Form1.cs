4810f0b304011716c9eac3e38ffde1e0c572d210
unknown <blue16@RM02-15.fasfg.local>
2015-07-06 09:41:26 +0100

    Finished XML assignment

--- XMLProject/Form1.cs
+++ XMLProject/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Xml;
 
 namespace XMLProject
 {
@@ -15,6 +17,25 @@ namespace XMLProject
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(@"cars.xml");
+            XmlTextReader xr = new XmlTextReader(sr);
+            XmlDocument carCollectionDoc = new XmlDocument();
+            carCollectionDoc.Load(xr);
+            linkLabel1.Text = carCollectionDoc.InnerText;
+            linkLabel2.Text = " First child node: " + carCollectionDoc.FirstChild.InnerText;
+            linkLabel3.Text = " Second child node: " + carCollectionDoc.FirstChild.NextSibling.InnerText;
+            XmlNode carcollection = carCollectionDoc.FirstChild.NextSibling;
+            linkLabel4.Text = "Now that we have a reference to 'carcollection': " + carcollection.FirstChild.InnerText;
+            linkLabel5.Text = "First child of carcollection: " + carcollection.FirstChild.InnerText;
+            linkLabel6.Text = "First child of the first child of carcollection: " + carcollection.FirstChild.FirstChild.InnerText;
+            XmlNodeList carCollectionItems = carCollectionDoc.SelectNodes("carcollection/car");
+            XmlNode make = carCollectionItems.Item(0).SelectSingleNode("make");
+            linkLabel7.Text = "make.InnerText: " + make.InnerText;
+
         }
     }
 }
