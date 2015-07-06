591dfac41a9c92d1c46696c518818157d98eac73
unknown <blue16@RM02-15.fasfg.local>
2015-07-06 09:15:46 +0100

    Started XML project

--- Assignment/CourseBooking/CheckButton.cs
+++ Assignment/CourseBooking/CheckButton.cs
@@ -8,20 +8,34 @@ using System.IO;
 
 namespace CourseBooking
 {
+    /// <summary>
+    /// Provides a "CheckBox" with the appearance of a size 30x30 button 
+    /// </summary>
     class CheckButton:CheckBox
     {
+        //used by CheckedChanged method to store the inital value of the Text 
+        //on the button
         private string initialText;
 
-        public CheckButton()
+        public CheckButton()  
         {
             this.Appearance = System.Windows.Forms.Appearance.Button;
+            //We always want the button size to be 30 x 30 but autosize takes 
+            //over unless min and max are set
             this.MaximumSize = new System.Drawing.Size(30, 30);
             this.MinimumSize = new System.Drawing.Size(30, 30);
             this.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.CheckedChanged += new System.EventHandler(this.CheckChanged);
         }
+
+        /// <summary>
+        /// When the Checked property changes this changes the style and 
+        /// text of the button 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CheckChanged(object sender, EventArgs e)
-        {
+        { 
             
             if (this.Checked)
             {
--- Assignment/CourseBooking/TestFileManipulation.cs
+++ Assignment/CourseBooking/TestFileManipulation.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace CourseBooking
 {
@@ -17,10 +18,177 @@ namespace CourseBooking
             InitializeComponent();
         }
 
+        private string[] text = File.ReadAllLines("courses.txt");
+        List<string> courseName = new List<string>();
+        List<string> courseBookedState = new List<string>();
+        List<string> courseDate = new List<string>();
+        List<string> courseCost = new List<string>();
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            string text = System.IO.File.ReadAllText(@"C:\Users\blue5.FASFG\Documents\vsprojects\Csharp-Basics\Assignment\courses.txt");
-            MessageBox.Show(text);
+            for (int i = 0; i < text.Length; i++)
+            {
+                text[i] = text[i].Trim('"'); 
+                //remove " from beginning and end of each line
+            }
+
+            for (int i = 0; i < text.Length; i += 4)
+            {
+                courseName.Add(text[i]);  
+                //add just the course titles into courseNames list                
+            }
+
+            courseName = courseName.Distinct().ToList<String>(); 
+            //remove any duplicate course titles
+
+            listBox1.Items.Clear(); 
+            //Ensures that the button doesn't keep adding to 
+            //the listbox everytime it's pressed
+
+            foreach (string item in courseName)
+            {
+                listBox1.Items.Add(item); 
+                //add the course titles to the listbox
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter starks = new StreamWriter("Values.txt");
+                starks.WriteLine("\"" + textBox1.Text + "\"");
+                starks.WriteLine("\"" + textBox2.Text + "\"");
+                starks.WriteLine("\"" + textBox3.Text + "\"");
+                starks.WriteLine("\"FFFFFFFFFFFF\"");
+                starks.Close();
+                MessageBox.Show("done");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("File save error");
+            }
+            var test = new TestFileManipulation();
+            test.Show();
+            this.Close();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.InitialDirectory = "C:\\Users\\blue16\\Documents\\vsprojects\\Csharp-Basics\\Assignment\\CourseBooking\\bin\\Debug\\";
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //Code to create a new file
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("File save error");
+                }
+            }
+            else if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                MessageBox.Show("File incorrect format or missing or dialog cancelled");
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "C:\\Users\\blue16\\Documents\\vsprojects\\Csharp-Basics\\Assignment\\CourseBooking\\bin\\Debug\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using ( StreamReader courseStream = new StreamReader(myStream))
+                        {
+                            string line;
+                            int linenumber = 0;
+
+                            while ((line = courseStream.ReadLine()) != null)
+                            {
+                                if(linenumber%4 == 3)
+                                {
+                                    courseBookedState.Add(line);
+                                }
+                                if (linenumber % 4 == 2)
+                                {
+                                    courseDate.Add(line);
+                                }
+                                if(linenumber%4 == 1)
+                                {
+                                    courseCost.Add(line);
+                                }
+                                if (linenumber % 4 == 1)
+                                {
+                                    courseCost.Add(line);
+                                }
+                                if (linenumber % 4 == 0)
+                                {
+                                    courseName.Add(line);
+                                }
+                                linenumber++;               
+                            }
+                            foreach (string item in courseBookedState)
+                            {
+                                foreach (char booked in item)
+                                {
+                                    if (booked == 'B')
+                                    {
+                                        //change checked state of first checkButton
+                                    }
+                                    else if (booked == 'F' || booked == '"')
+                                    {
+                                        //Do nothing, the default state is unchecked
+                                    }
+                                    else
+                                    {
+                                        MessageBox.Show("Error: file format error");
+                                    }
+                                }
+                            }
+                            foreach (string cDate in courseDate)
+                            {
+                                try
+                                {
+                                    //cDate.DateCheck();
+                                    //DateCheck validates the date
+                                    //change the date label's Text to cDate
+                                }
+                                catch (Exception)
+                                {
+                                    MessageBox.Show("Error: Invalid Date");
+                                }
+                            }
+                            foreach (string cName in courseName)
+                            {
+                                //no particular error handling needed
+                                //change the value of the label
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
         }
     }
 }
